469f43c1351ee9a32a3d54d6bb5426e183a7835e
Diego Opel <diego.opel@gmail.com>
2023-02-02 17:48:20 -0300

    Resolucion a desafio clase 6

---
+++ src/productManager.js
@@ -0,0 +1,80 @@
+import fs from "fs";
+import { v4 as uuidv4 } from 'uuid';
+
+class ProductManager {
+  constructor(path){
+    this.products = [];
+    this.path = path;
+  }
+  async #checkDB(){
+    if(fs.existsSync(this.path)){
+      this.products = JSON.parse(await fs.promises.readFile(this.path))
+    }
+  }
+  async #updateDB(){
+    await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+  }
+  async addProduct(title, description, price, thumbnail, code, stock){
+    await this.#checkDB()
+    const isInArray = this.products.some(product => product.code === code)
+    if(isInArray === false && title && description && price && thumbnail && stock){
+      this.products.push({
+        id: uuidv4(),
+        title: title,
+        description: description,
+        price: price,
+        thumbnail: thumbnail,
+        code: code,
+        stock: stock
+      });
+      this.#updateDB
+      return "Producto agregado"
+    }else{
+      return "Producto repetido o faltan características"
+    }
+  }
+  async getProducts(){
+    await this.#checkDB()
+    return this.products
+  }
+  async getProductById(id){
+    await this.#checkDB()
+    const productFound = this.products.find(product => product.id === id)
+    if (productFound){
+      return productFound
+    }else{
+      return "Product not found"
+    }
+  }
+  async updateProduct(id, title, description, price, thumbnail, code, stock){
+    await this.#checkDB()
+    const indexFound = this.products.findIndex(product => product.id === id)
+    if(indexFound !== -1){
+      this.products[indexFound] = {
+        id: id,
+        title: title,
+        description: description,
+        price: price,
+        thumbnail: thumbnail,
+        code: code,
+        stock: stock
+      }
+      await this.#updateDB()
+      return "Producto actualizado"
+    }else{
+      return "Not found"
+    }
+  }
+  async deleteProduct(id){
+    await this.#checkDB()
+    const indexFound = this.products.findIndex(product => product.id === id)
+    if(indexFound !== -1){
+      this.products.splice(indexFound,indexFound+1)
+      await this.#updateDB()
+      return "Producto eliminado"
+    }else{
+      return "Not found"
+    }
+  }
+}
+export const productManager = new ProductManager("./src/products.json");
